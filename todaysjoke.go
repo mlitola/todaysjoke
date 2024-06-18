9d7cc7ee2adfb05baac6afd78fa23c11f5175799
Mikko Litola <mikko.litola@gmail.com>
2024-06-18 11:39:51 +0300

    Small clean-ups

--- todaysjoke.go
+++ todaysjoke.go
@@ -10,32 +10,34 @@ import (
 
 const jokeUrl = "https://icanhazdadjoke.com/"
 
+const userAgent = "Todays Joke (https://github.com/mlitola/todaysjoke)"
+
 func main() {
 	client := &http.Client{}
 
 	req, err := http.NewRequest("GET", jokeUrl, nil)
 	if err != nil {
-		fmt.Printf("error initializing a new http request: %s\n", err)
+		fmt.Printf("Error initializing a new http request: %s\n", err)
 		os.Exit(1)
 	}
 
-	req.Header.Set("User-Agent", "Todays Joke (https://github.com/mlitola/todaysjoke)")
+	req.Header.Set("User-Agent", userAgent)
 	req.Header.Set("Accept", "text/plain")
 
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("error executing a http request: %s\n", err)
+		fmt.Printf("Error making a http request: %s\n", err)
 		os.Exit(1)
 	}
 
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Printf("i/o error when reading the response body: %s\n", err)
+		fmt.Printf("I/O error while reading the response body: %s\n", err)
 		os.Exit(1)
 	}
 
 	currentTime := time.Now()
-	fmt.Printf("%s (%s):\n\n", "Todays joke", currentTime.Format("02.01.2006"))
+	fmt.Printf("%s (%s):\n\n", "Today's joke", currentTime.Format("02.01.2006"))
 	fmt.Printf("%s\n\n", body)
 }
